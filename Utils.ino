025b89f47708a081a9224af5db455a85d8aba3ad
koki3141 <100524649+koki3141@users.noreply.github.com>
2023-11-28 16:55:43 +0900

    servoモータの制御をServoに変更

---
+++ Utils.ino
@@ -0,0 +1,8 @@
+void runStand(){
+}
+
+// void servoAngleSet90(){
+//   for (int i = 0; i < 12; i++) {
+//     servoMove(SERVOs[i].pin,90);
+//   }
+// }
--- main.ino
+++ main.ino
@@ -1,6 +1,6 @@
 #include <Wire.h>
 #include <Adafruit_PWMServoDriver.h>
-
+#include <Servo.h>
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x40);
 
 const int SERVOMIN = 500;
@@ -13,43 +13,67 @@ const int TRIG_PIN_LEFT = 5;
 // We may not use it.”
 const int forward_right_1 = 0;
 const int forward_right_2 = 1;
-const int forward_left_1 = 2;
-const int forward_left_2 = 3;
-const int middle_right_1 = 4;
-const int middle_right_2 = 5;
-const int middle_left_1 = 6;
-const int middle_left_2 = 7;
-const int back_right_1 = 8;
-const int back_right_2 = 9;
-const int back_left_1 = 10;
-const int back_left_2 = 11;
+const int forward_right_3 = 2;
+
+const int forward_left_1 = 3;
+const int forward_left_2 = 4;
+const int forward_left_3 = 5;
+
+const int back_right_1 = 6;
+const int back_right_2 = 7;
+const int back_right_3 = 8;
+
+const int back_left_1 = 9;
+const int back_left_2 = 10;
+const int back_left_3 = 11;
 
 enum status {
   pk,
   clime,
   perfomance,
-  stand
+  test
 };
 
 typedef struct {
-  int pin;
-  int stand; //stand angle
-} servo;
+  Servo servo;
+  bool reverse;
+} oneServo;
 
-const servo SERVOs[12] = {
-  { 0 , 0},
-  { 1 , 0},
-  { 2 , 0},
-  { 4 , 0},//servopin 3 is broken in Japan
-  { 5 , 0},
-  { 6 , 0},
-  { 7 , 0},
-  { 8 , 0},
-  { 9 , 0},
-  { 10 , 0},
-  { 11 , 0},
-  { 12 , 0}
+typedef struct {
+  oneServo join1;
+  oneServo join2;
+  oneServo join3;
+} oneLeg;
+
+const oneLeg FOWARD_RIGHT_LEG = {
+  {Servo(), false},
+  {Servo(), false},
+  {Servo(), false}
 };
+
+const oneLeg FOWARD_LEFT_LEG = {
+  {Servo(), true},
+  {Servo(), false},
+  {Servo(), false}
+};
+const oneLeg BACK_RIGHT_LEG = {
+  {Servo(), false},
+  {Servo(), false},
+  {Servo(), false}
+};
+const oneLeg BACK_LEFT_LEG = {
+  {Servo(), true},
+  {Servo(), false},
+  {Servo(), false}
+};
+
+typedef struct {
+  int join1;
+  int join2;
+  int join3;
+} legAngle;
+
+const legAngle STANDARD = {90,90,90};
 
 
 void setup() {
@@ -57,6 +81,23 @@ void setup() {
   pinMode(TRIG_PIN_RIGHT, OUTPUT);
   pinMode(ECHO_PIN_LEFT, INPUT);
   pinMode(TRIG_PIN_LEFT, OUTPUT);
+
+  FOWARD_RIGHT_LEG.join1.servo.attach(0);
+  FOWARD_RIGHT_LEG.join2.servo.attach(1);
+  FOWARD_RIGHT_LEG.join3.servo.attach(2);
+  
+  FOWARD_LEFT_LEG.join1.servo.attach(3);
+  FOWARD_LEFT_LEG.join2.servo.attach(4);
+  FOWARD_LEFT_LEG.join3.servo.attach(5);
+
+  BACK_RIGHT_LEG.join1.servo.attach(6) ;
+  BACK_RIGHT_LEG.join2.servo.attach(7);
+  BACK_RIGHT_LEG.join3.servo.attach(8);
+
+  BACK_LEFT_LEG.join1.servo.attach(9);
+  BACK_LEFT_LEG.join2.servo.attach(10);
+  BACK_LEFT_LEG.join3.servo.attach(11);
+
   pwm.begin();
   pwm.setPWMFreq(50);
   Serial.begin(9600);
@@ -74,8 +115,8 @@ void exe(status type) {
     case perfomance:
       runPerformance();
       break;
-    case stand:
-      runStand();
+    case test:
+      runtest();
     default:
       break;
   }
@@ -86,14 +127,26 @@ void loop() {
   exe(pk);
 }
 
+void runtest(){
 
-void runStand(){
-  for (int i = 0; i < 12; i++) {
-    servoMove(SERVOs[i].pin,SERVOs[i].stand);
-  }
 }
 
-void servoMove(int pin, int set_angle){
-  int angle = map(set_angle, 0, 180, SERVOMIN, SERVOMAX);
-  pwm.setPWM(pin, 0, angle);
+void moveAllLeg(legAngle& leg_angle){
+  moveOneLeg(FOWARD_RIGHT_LEG, leg_angle);
+  moveOneLeg(FOWARD_LEFT_LEG, leg_angle);
+  moveOneLeg(BACK_RIGHT_LEG, leg_angle);
+  moveOneLeg(BACK_LEFT_LEG, leg_angle);
+}
+
+void moveOneLeg(const oneLeg& one_leg, const legAngle& leg_angle){
+    servoMove(one_leg.join1, leg_angle.join1);
+    servoMove(one_leg.join2, leg_angle.join2);
+    servoMove(one_leg.join3, leg_angle.join3);
+}
+
+void servoMove(const oneServo &join, int set_angle){
+  if (join.reverse){
+    set_angle = abs(set_angle - STANDARD.join1);
+  }
+  join.servo.write(set_angle);
 }
